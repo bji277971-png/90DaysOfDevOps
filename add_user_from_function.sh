daa951e9011029f0f53a4b3b53f5eea164c72dde
balaji123 <balaji21@gmail.com>
2026-04-12 03:06:36 +0000

    moved scripts to batch-10 folder

--- add_user_from_function.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source "./creat_user.sh"
-
-# function call
-creat_user
--- create_simple_file.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-touch new-file.txt && echo "file bn gyi" || { echo "file nhi h ya nhi bani ya koi issue h" ; echo "yaar sach me error h";}
